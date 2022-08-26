631003b0d7b4c6102f87fe4fd7c6fd0ac6b08b3e
Ivan42210 <ivan.cha.42@outlook.fr>
2022-08-26 12:51:42 +0200

    dernières modifs

---
+++ scripts/factories/photo.js
@@ -0,0 +1,43 @@
+class photoFactory {
+    constructor(data) {
+        this._id = data.id
+        this._photographerId = data.photographerId
+        this._title = data.title
+        this._image = data.image
+        this._video = data.video
+        this._likes = data.likes
+        this._date = data.date
+        this._price = data.price
+    }
+
+    get id() {
+        return this._id
+    }
+
+    get photographerId() {
+        return this._photographerId
+    }
+
+    get title() {
+        return this._title
+    }
+
+    get likes() {
+        return this._likes
+    }
+
+    get date() {
+        return this._date
+    }
+
+    get type() {
+        if (this._image) {
+            return 'image'
+        } else if (this._video) {
+            return 'video'
+        } else {
+            return 'none'
+        }
+    }
+
+}

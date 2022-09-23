77b7e2694c390ba03b5a0951fcb877ad2ceb62f9
Ivan42210 <ivan.cha.42@outlook.fr>
2022-09-24 00:24:26 +0200

    new changes

---
+++ scripts/utils/myLightbox.js
@@ -0,0 +1,13 @@
+let lastImage = ''
+let click = 0
+
+function displayLightbox {
+    const lightboxContainer = document.getElementById('lightbox')
+    const lightboxImage = document.querySelector('.lightbox__img')
+    const leftArrow = document.getElementById('left-arrow')
+    const rightArrow = document.getElementById('right-arrow')
+    const closeButton = document.getElementById('close')
+
+
+
+}
